e6425e073cad7689004af567bb023aa9008d64a6
Riddhima Deshmukh <115169787+ridds-io@users.noreply.github.com>
2026-04-08 20:51:24 +0530

    Add Celsius to Fahrenheit conversion function
    Implement Celsius to Fahrenheit conversion function.

---
+++ Mathematics/CelciusToFahrenheit.py
@@ -0,0 +1,10 @@
+'''
+Given a temperature in celsius C. You need to convert the given temperature into Fahrenheit.
+
+Constraints:
+1 ≤ C ≤ 104
+'''
+
+class Solution:
+    def cToF(self,C):
+        return (C*(9/5))+32
