a14f10e6853b0e4aff8e1b4f90b9d85f5437cf32
Riddhima Deshmukh <115169787+ridds-io@users.noreply.github.com>
2026-04-17 00:04:21 +0530

    Create GPTerm.py

---
+++ Mathematics/GPTerm.py
@@ -0,0 +1,14 @@
+'''
+Given the first 2 terms a and b of a Geometric Series. The task is to find the nth term of the series.
+
+Constraints:
+
+-100 ≤ a, b ≤ 100
+1 ≤ n ≤ 9
+
+'''
+
+class Solution:
+    def termOfGP(self, a, b, n):
+        r = b / a
+        return a * (r**(n-1))
